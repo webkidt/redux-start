5b59a10beb53a1a7aabd9cebaeba7fb2b79c6b98
David Pam <niratpam@gmail.com>
2020-08-27 04:01:51 +0100

    Updating Arrays Immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -26,7 +26,10 @@ class Counter extends Component {
         <button onClick={this.props.onStoreResult}>Store Result</button>
         <ul>
           {this.props.storedResults.map((strResult) => (
-            <li key={strResult.id} onClick={this.props.onDeleteResult}>
+            <li
+              key={strResult.id}
+              onClick={() => this.props.onDeleteResult(strResult.id)}
+            >
               {strResult.value}
             </li>
           ))}
@@ -50,7 +53,7 @@ const mapDispatchToProps = (dispatch) => {
     onAddCounter: () => dispatch({ type: 'ADD', val: 10 }),
     onSubtractCounter: () => dispatch({ type: 'SUBTRACT', val: 15 }),
     onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
-    onDeleteResult: () => dispatch({ type: 'DELETE_RESULT' }),
+    onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultElId: id }),
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -30,6 +30,17 @@ const reducer = (state = initialState, action) => {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter }),
       };
+    case 'DELETE_RESULT':
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+      const updatedArray = state.results.filter(
+        (result) => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray,
+      };
     default:
       return state;
   }
