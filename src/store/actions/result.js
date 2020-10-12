f70c061abe2e8679e3644c92fc80cadedc954775
David Pam <niratpam@gmail.com>
2020-10-12 01:31:02 +0100

    Where to Put Data Transforming Logic

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,6 +1,7 @@
 import * as actionTypes from './actionTypes';
 
 export const saveResult = (result) => {
+  // const updatedResult = result * 2;
   return {
     type: actionTypes.STORE_RESULT,
     result,
